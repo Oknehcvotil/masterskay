32859083462f525d8b1ad782a202caa141901d03
Kirill <121458409+Oknehcvotil@users.noreply.github.com>
2024-02-28 01:09:19 +0200

    i18n
    add i18n to meta tags

--- src/lib/utils/dictionaries.ts
+++ src/lib/utils/dictionaries.ts
@@ -1,8 +1,6 @@
 import "server-only";
 import type { Locale } from "@/src/lib/utils/i18n-config";
 
-// We enumerate all dictionaries here for better linting and typescript support
-// We also get the default import for cleaner types
 const dictionaries = {
   ua: () => import("../dictionaries/ua.json").then((module) => module.default),
   en: () => import("../dictionaries/en.json").then((module) => module.default),
