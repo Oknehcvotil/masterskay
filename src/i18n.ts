9f402e0374aaaab7a528a4bf8e485213bac2bd8b
Kirill <121458409+Oknehcvotil@users.noreply.github.com>
2024-03-23 20:10:02 +0200

    Update i18n.ts

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,15 +1,23 @@
 import { notFound } from "next/navigation";
 import { getRequestConfig } from "next-intl/server";
-
-// Can be imported from a shared config
-const locales = ["ua", "ru"];
+import { locales } from "./config";
 
 export default getRequestConfig(async ({ locale }) => {
   // Validate that the incoming `locale` parameter is valid
   if (!locales.includes(locale as any)) notFound();
 
+  let messages;
+  if (locale === "ua") {
+    // When using Turbopack, this will enable HMR for `en`
+    const uaMessages = await import("./messages/ua.json");
+    messages = uaMessages.default;
+  } else {
+    const ruMessages = await import("./messages/ru.json");
+    messages = ruMessages.default;
+  }
+
   return {
-    messages: (await import(`./messages/${locale}.json`)).default,
+    messages: messages,
   };
 });
 
