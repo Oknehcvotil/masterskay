7c19926c7a989f47ff156283e035de6093630bb1
Kirill <121458409+Oknehcvotil@users.noreply.github.com>
2024-03-23 18:54:34 +0200

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -3,14 +3,14 @@ import { Pathnames } from "next-intl/navigation";
 export const locales = ["ua", "ru"] as const;
 
 export const pathnames = {
-  "/": "/ua", 
+  "/": "/",
   "/pathnames": {
     ua: "/ua",
     ru: "/ru",
   },
 } satisfies Pathnames<typeof locales>;
 
-// Use the default: `always`
+// Используйте по умолчанию: `always`
 export const localePrefix = undefined;
 
 export type AppPathnames = keyof typeof pathnames;
